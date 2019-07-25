6f28ed306493508603c5abc148e2e8a2461da030
oiaPY <nanfang7092180@qq.com>
2019-07-25 18:56:45 +0800

    1.修改在在f.readline()的时候会读\n，而导致结果无论输入对错都会报密码错误的问题----用.strip（"\n"）来去除换行符 2.重新整理了下在没有达到三次尝试/已经达到三次尝试，输入用户名密码错误的逻辑--多增加关于计数i的一个if判断

--- access.py
+++ access.py
@@ -4,20 +4,19 @@
 
 # 创建存放用户信息的txt文件，并写入用户名和密码，分列两行
 f = open("user.txt", "w")
-f.write("moziran")
-f.write("\n3614")
-# f.close()
+# f.writelines(["moziran", "\n3614"])
+f.write("moziran\n")
+f.write("3614")
+f.close()
 
 # 打开文件，分别读两行的内容，并存到两个变量中，用于之后的比较
 f = open("user.txt", "r")
-# f.write("moziran")
-# f.write("\n361426491")
-name = f.readline()
+name = f.readline().strip('\n')
 passwd = f.readline()
 # print(type(name))
 # print(type(passwd))
-# print(name)
-# print(passwd)
+print(name)
+print(passwd)
 f.close()
 
 # 执行for循环，三次退出，提示用户账号密码锁定
@@ -34,7 +33,11 @@ for i in range(3):
         print("欢迎进入系统！！")
         break
     else:
-        print("你输入的用户名或密码错误！")
-        continue
-# 循环结束提示用户账号被锁定
-print("你的账户已被锁定，请稍后再试！")
+        if i == 2:
+    # 循环结束提示用户账号被锁定
+            print("你的账户已被锁定，请稍后再试！")
+        else:
+    # 3次机会（循环还在），提示密码错误
+            print("你输入的用户名或密码错误！")
+
+
