fa6da2f481fc035ee788850890390365e2f2c113
oiaPY <nanfang7092180@qq.com>
2019-10-08 08:55:57 +0800

    1.解决了完成添加商品不能退出的问题（0的str转换） 2.添加了最终商品确认的print 3.商品总价和余额的比较，分别输出‘余额不足’或者‘交易成功’

--- My_cart.py
+++ My_cart.py
@@ -4,20 +4,34 @@
 
 # 这是一个购物商城程序
 # 商品展示
-Product = {'1': ['a', 50], '2': ['b', 70], '3': ['c', 90], '4': ['d', 100], }
+# Product = {'1': ['a', 50], '2': ['b', 70], '3': ['c', 90], '4': ['d', 100], }
+Product = {'a': 50, 'b':  70, 'c': 90, 'd':  100, }
+print(Product)
 # 个人账户余额
 Account_Balance = 300
+Cart_Price = 0
 
 # 购买商品，先建立一个空的购物车,初始价格为0
 Cart_dic = []
+Cart_Flag = True
 Cart = input("请加入你选择的商品编号(若已完成添加请输入0)：")
-while Cart != 0:
-    if Cart == 0:
+while Cart_Flag:
+    if Cart == str(0):     #这里0 是默认int型，要转换成str才能比较
         break
     else:
+        print(Product[Cart])
+        Cart_Price = Cart_Price + Product[Cart]
+
+
         Cart_dic.append(Cart)
         Cart = input("请加入你选择的商品编号(若已完成添加请输入0)：")
-# print(Cart_dic)
+print('请确认您购买的商品是：'+str(Cart_dic))
+
+if Cart_Price > Account_Balance:
+    print('余额不足，请充值！')
+else:
+    Account_Balance = Account_Balance - Cart_Price
+    print("交易成功,账户余额为" + str(Account_Balance))
 
 
 # Cart_Price = 0
